28e512c004831b9cbb9689dcda290ad479fd04b5
jhceation <rainymanson@gmail.com>
2024-08-09 13:19:07 +0900

    13:19

--- app/routes/home/main/Main1_4.tsx
+++ app/routes/home/main/Main1_4.tsx
@@ -61,7 +61,7 @@ export default function Main1_4 () {
     <div className="bg-gray-600">
       <section ref={containerRef} className='h-[800lvh]'>
         <div ref={sectRef} className="sticky h-lvh top-0 flex items-center justify-center overflow-hidden">
-          <div className="h-svh w-full flex flex-col justify-between pt-nav ">
+          <div className="h-svh w-full flex flex-col justify-between pt-nav-m md:pt-nav ">
             <div className="font-[Pretendard] font-black leading-[1] text-[20px] md:text-[60px] uppercase">
               <h1 className="">so we built</h1>
               <h1 className="">web scrolling</h1>
